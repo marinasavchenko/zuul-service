5bcefe2e5fd1e1e3f8c36f3b1b74ccbeec24b304
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-19 13:03:43 +0100

    added FilterUtils

---
+++ src/main/java/com/onlinestore/zuulservice/filters/FilterUtils.java
@@ -0,0 +1,9 @@
+package com.onlinestore.zuulservice.filters;
+
+public class FilterUtils {
+	public static final String CORRELATION_ID = "onlst-correlation-id";
+	public static final String AUTH_TOKEN = "onlst-auth-token";
+	public static final String USER_ID = "onlst-user-id";
+	public static final String CUSTOMER_ID = "onlst-customer-id";
+	public static final String PRE_FILTER_TYPE = "pre";
+}
