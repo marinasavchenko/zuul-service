8e70670b252a7bcbdb2c96a26388f39d2b11d085
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-19 13:27:53 +0100

    added get, setCorrelationId()

--- src/main/java/com/onlinestore/zuulservice/filters/FilterUtils.java
+++ src/main/java/com/onlinestore/zuulservice/filters/FilterUtils.java
@@ -1,9 +1,26 @@
 package com.onlinestore.zuulservice.filters;
 
+import com.netflix.zuul.context.RequestContext;
+
 public class FilterUtils {
 	public static final String CORRELATION_ID = "onlst-correlation-id";
 	public static final String AUTH_TOKEN = "onlst-auth-token";
 	public static final String USER_ID = "onlst-user-id";
 	public static final String CUSTOMER_ID = "onlst-customer-id";
 	public static final String PRE_FILTER_TYPE = "pre";
+
+	public String getCorrelationId() {
+		RequestContext currentContext = RequestContext.getCurrentContext();
+
+		if (currentContext.getRequest().getHeader(CORRELATION_ID) != null) {
+			return currentContext.getRequest().getHeader(CORRELATION_ID);
+		} else {
+			return currentContext.getZuulRequestHeaders().get(CORRELATION_ID);
+		}
+	}
+
+	public void setCorrelationId(String correlationId) {
+		RequestContext currentContext = RequestContext.getCurrentContext();
+		currentContext.addZuulRequestHeader(CORRELATION_ID, correlationId);
+	}
 }
