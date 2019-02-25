8a06b7f36a6a9df2a093a2ed6e6a4191d4c144fc
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-25 09:59:51 +0100

    added getRouteRecordInfo()

--- src/main/java/com/onlinestore/zuulservice/filters/AlternativeRoutesFilter.java
+++ src/main/java/com/onlinestore/zuulservice/filters/AlternativeRoutesFilter.java
@@ -2,10 +2,16 @@ package com.onlinestore.zuulservice.filters;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
+import com.onlinestore.zuulservice.model.RouteRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 public class AlternativeRoutesFilter extends ZuulFilter {
+	private static final String ROUTE_RECORD_URI = "http://alternativeroutesservice/v1/route/records/{serviceName}";
 	private static final int FILTER_ORDER = 1;
 	private static final boolean SHOULD_FILTER = true;
 
@@ -36,5 +42,16 @@ public class AlternativeRoutesFilter extends ZuulFilter {
 	@Override
 	public Object run() throws ZuulException {
 		return null;
+	}
+
+	private RouteRecord getRouteRecordInfo(String serviceName) {
+		ResponseEntity<RouteRecord> responseEntity;
+		try {
+			responseEntity = restTemplate.exchange(ROUTE_RECORD_URI, HttpMethod.GET, null, RouteRecord.class, serviceName);
+		} catch (HttpClientErrorException exception) {
+			if (exception.getStatusCode() == HttpStatus.NOT_FOUND) return null;
+			throw exception;
+		}
+		return responseEntity.getBody();
 	}
 }
