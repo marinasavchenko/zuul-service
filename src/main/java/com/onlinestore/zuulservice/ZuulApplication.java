e5d0e9e23ca42d089ccdfb69e68b1be247185ec4
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-05 09:11:13 +0100

    added getRestTemplate()

--- src/main/java/com/onlinestore/zuulservice/ZuulApplication.java
+++ src/main/java/com/onlinestore/zuulservice/ZuulApplication.java
@@ -1,12 +1,35 @@
 package com.onlinestore.zuulservice;
 
+import com.onlinestore.zuulservice.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+		if (CollectionUtils.isEmpty(interceptors)) {
+			interceptors = new ArrayList<>();
+		}
+		interceptors.add(new UserContextInterceptor());
+		restTemplate.setInterceptors(interceptors);
+		return restTemplate;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
