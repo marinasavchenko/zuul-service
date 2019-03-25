97819b3143f146d2934dc766027e18162bcfc906
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 10:19:14 +0100

    added doc comments to ZuulApplication

--- src/main/java/com/onlinestore/zuulservice/ZuulApplication.java
+++ src/main/java/com/onlinestore/zuulservice/ZuulApplication.java
@@ -13,10 +13,18 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that bootstraps this service.
+ */
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication {
-
+	/**
+	 * Creates {@code RestTemplate} and adds {@code UserContextInterceptor} to it.
+	 * This {@code RestTemplate} is going to use Ribbon.
+	 *
+	 * @return {@code RestTemplate}
+	 */
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
@@ -31,6 +39,11 @@ public class ZuulApplication {
 		return restTemplate;
 	}
 
+	/**
+	 * Main method, used to run this application.
+	 *
+	 * @param args the string array, that contains command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
