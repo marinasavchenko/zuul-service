d0f60d778be0bcb0d3af5e60f9a877edd368be96
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-25 10:58:04 +0100

    added doc comments to UserContext

--- src/main/java/com/onlinestore/zuulservice/utils/UserContext.java
+++ src/main/java/com/onlinestore/zuulservice/utils/UserContext.java
@@ -2,18 +2,50 @@ package com.onlinestore.zuulservice.utils;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Contextual information scrapped from incoming HTTP request.
+ */
 @Component
 public class UserContext {
-
+	/**
+	 * CORRELATION_ID HTTP request header.
+	 */
 	public static final String CORRELATION_ID = "onlst-correlation-id";
+	/**
+	 * AUTH_TOKEN HTTP request header.
+	 */
 	public static final String AUTH_TOKEN = "onlst-auth-token";
+	/**
+	 * USER_ID HTTP request header.
+	 */
 	public static final String USER_ID = "onlst-user-id";
+	/**
+	 * CUSTOMER_ID HTTP request header.
+	 */
 	public static final String CUSTOMER_ID = "onlst-customer-id";
 
+	/**
+	 * Correlation Id used to trace the chain of events.
+	 */
 	private String correlationId = new String();
+	/**
+	 * Auth token.
+	 */
 	private String authToken = new String();
+	/**
+	 * User id.
+	 */
 	private String userId = new String();
+	/**
+	 * Unique id of the customer.
+	 */
 	private String customerId = new String();
+
+	/**
+	 * Constructs new empty {@code UserContext} instance.
+	 */
+	public UserContext() {
+	}
 
 	public String getCorrelationId() {
 		return correlationId;
