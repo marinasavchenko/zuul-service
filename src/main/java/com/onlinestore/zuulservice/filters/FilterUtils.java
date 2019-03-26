8d394606236de6e73211d820be842d34ee472164
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-26 13:31:50 +0100

    added doc comments to FilterUtils

--- src/main/java/com/onlinestore/zuulservice/filters/FilterUtils.java
+++ src/main/java/com/onlinestore/zuulservice/filters/FilterUtils.java
@@ -3,16 +3,50 @@ package com.onlinestore.zuulservice.filters;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.stereotype.Component;
 
+/**
+ * {@code FilterUtils} class is used to encapsulate common functionality used by all filters of this service.
+ */
+//TODO: refactor repeated code.
 @Component
 public class FilterUtils {
+	/**
+	 * CORRELATION_ID HTTP request header.
+	 */
 	public static final String CORRELATION_ID = "onlst-correlation-id";
+	/**
+	 * AUTH_TOKEN HTTP request header.
+	 */
 	public static final String AUTH_TOKEN = "onlst-auth-token";
+	/**
+	 * USER_ID HTTP request header.
+	 */
 	public static final String USER_ID = "onlst-user-id";
+	/**
+	 * CUSTOMER_ID HTTP request header.
+	 */
 	public static final String CUSTOMER_ID = "onlst-customer-id";
+
+	/**
+	 * Pre filter type.
+	 */
 	public static final String PRE_FILTER_TYPE = "pre";
+	/**
+	 * Post filter type.
+	 */
 	public static final String POST_FILTER_TYPE = "post";
+	/**
+	 * Route filter type.
+	 */
 	public static final String ROUTE_FILTER_TYPE = "route";
 
+	/**
+	 * Gets correlation id.
+	 * <p>
+	 * Checks whether correlation id is set on the HTTP Headers for the incoming request.
+	 * If it isn’t there, check the ZuulRequestHeaders.
+	 *
+	 * @return correlation id
+	 */
 	public String getCorrelationId() {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 
@@ -23,11 +57,24 @@ public class FilterUtils {
 		}
 	}
 
+	/**
+	 * Adds correlation id value to the HTTP request headers.
+	 * <p>
+	 * {@code addZuulRequestHeader} maintains a separate map of HTTP headers.
+	 * The data will be merged, when the target service is invoked by your Zuul server
+	 *
+	 * @param correlationId
+	 */
 	public void setCorrelationId(String correlationId) {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		currentContext.addZuulRequestHeader(CORRELATION_ID, correlationId);
 	}
 
+	/**
+	 * Gets customer id.
+	 *
+	 * @return customer id
+	 */
 	public final String getCustomerId() {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		if (currentContext.getRequest().getHeader(CUSTOMER_ID) != null) {
@@ -37,11 +84,21 @@ public class FilterUtils {
 		}
 	}
 
+	/**
+	 * Sets customer id.
+	 *
+	 * @param customerId
+	 */
 	public void setCustomerId(String customerId) {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		currentContext.addZuulRequestHeader(CUSTOMER_ID, customerId);
 	}
 
+	/**
+	 * Gets user id.
+	 *
+	 * @return user id
+	 */
 	public final String getUserId() {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		if (currentContext.getRequest().getHeader(USER_ID) != null) {
@@ -51,16 +108,31 @@ public class FilterUtils {
 		}
 	}
 
+	/**
+	 * Sets user id.
+	 *
+	 * @param userId
+	 */
 	public void setUserId(String userId) {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		currentContext.addZuulRequestHeader(USER_ID, userId);
 	}
 
+	/**
+	 * Gets authentication token.
+	 *
+	 * @return auth token
+	 */
 	public final String getAuthToken() {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		return currentContext.getRequest().getHeader(AUTH_TOKEN);
 	}
 
+	/**
+	 * Gets id of a service.
+	 *
+	 * @return id of a service
+	 */
 	public String getServiceId() {
 		RequestContext currentContext = RequestContext.getCurrentContext();
 		if (currentContext.get("serviceId") == null) return "";
