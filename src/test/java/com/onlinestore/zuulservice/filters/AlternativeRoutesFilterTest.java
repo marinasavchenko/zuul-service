513ce1f239bcf4090c8edd5f336ae471aa1f951a
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-24 15:18:00 +0200

    added shouldReturnTrueWhenActiveStatusYes() test

--- src/test/java/com/onlinestore/zuulservice/filters/AlternativeRoutesFilterTest.java
+++ src/test/java/com/onlinestore/zuulservice/filters/AlternativeRoutesFilterTest.java
@@ -1,14 +1,13 @@
 package com.onlinestore.zuulservice.filters;
 
 import com.onlinestore.zuulservice.domain.RouteRecord;
+import com.onlinestore.zuulservice.utils.RandomGenerator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Random;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -28,16 +27,25 @@ public class AlternativeRoutesFilterTest {
 	private RestTemplate restTemplate;
 
 	@Mock
-	private Random random;
+	private RandomGenerator randomGenerator;
 
 	@Before
 	public void setUp() throws Exception {
-		alternativeRoutesFilter = new AlternativeRoutesFilter(filterUtils, restTemplate);
+		alternativeRoutesFilter = new AlternativeRoutesFilter(filterUtils, restTemplate, randomGenerator);
 	}
 
 	@Test
 	public void shouldReturnFalseWhenActiveStatusNO() throws Exception {
 		when(routeRecord.getActiveStatus()).thenReturn("NO");
 		assertThat(alternativeRoutesFilter.shouldUseAlternativeRoute(routeRecord)).isFalse();
+	}
+
+	@Test
+	public void shouldReturnTrueWhenActiveStatusYes() throws Exception {
+		when(routeRecord.getActiveStatus()).thenReturn("YES");
+		when(routeRecord.getWeight()).thenReturn(3);
+		when(randomGenerator.getRandomInt()).thenReturn(4);
+
+		assertThat(alternativeRoutesFilter.shouldUseAlternativeRoute(routeRecord)).isTrue();
 	}
 }
